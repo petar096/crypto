8214895457f68cc70ebf40304fa31b4fa8050d6e
Petar <petar.pavlovic@quantox.com>
2019-03-29 14:55:10 +0100

    Improved design

--- src/App.js
+++ src/App.js
@@ -20,14 +20,36 @@ class App extends Component {
 		limit: 5,
 		activeListOfCurrencies: [],
 		myBalance: [],
-		activePage: 1
+		activePage: 1,
+		loading: false
 	};
 
 	componentDidMount() {
+		this.getCryptoCurrencies();
+
+		setInterval(() => {
+			this.setState(
+				{
+					currencies: [],
+					selected: '',
+					current: 0,
+					limit: 5,
+					activeListOfCurrencies: [],
+					myBalance: [],
+					activePage: 1
+				},
+				() => this.getCryptoCurrencies()
+			);
+		}, 30000);
+	}
+
+	getCryptoCurrencies = () => {
+		this.setState({ loading: true });
 		axios
 			.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd')
 			.then(({ data }) => {
 				let initialBalance;
+
 				if (localStorage.getItem('myBalance')) {
 					initialBalance = JSON.parse(localStorage.getItem('myBalance'));
 				} else {
@@ -37,12 +59,15 @@ class App extends Component {
 				this.setState(
 					{
 						currencies: data,
-						myBalance: [...this.state.myBalance, ...initialBalance]
+						myBalance: [...this.state.myBalance, ...initialBalance],
+						loading: false
 					},
-					() => this.getListOfCurrencies()
+					() => {
+						this.getListOfCurrencies();
+					}
 				);
 			});
-	}
+	};
 
 	handleOnChnage = e => {
 		this.setState({
@@ -89,99 +114,106 @@ class App extends Component {
 
 	render() {
 		return (
-			<div className="App">
-				<Header as="h1">First Header</Header>
+			<Container textAlign="center">
+				<div className="App">
+					<Header as="h1">First Header</Header>
 
-				<Pagination
-					activePage={this.state.activePage}
-					boundaryRange={0}
-					ellipsisItem={null}
-					onPageChange={(e, { activePage }) => {
-						this.setState({ activePage }, () => this.getListOfCurrencies());
-					}}
-					totalPages={this.state.currencies.length / this.state.limit}
-				/>
+					{this.state.loading ? (
+						<h1>loding..</h1>
+					) : (
+						<>
+							<Form>
+								<Form.Field inline>
+									<label>Limit</label>
+									<Input
+										placeholder="First name"
+										onChange={e => this.setState({ limit: e.target.value })}
+									/>
 
-				<Input
-					name="limit"
-					onChange={e => this.setState({ limit: e.target.value })}
-				/>
+									<Button onClick={() => this.getListOfCurrencies()} secondary>
+										Submit
+									</Button>
+								</Form.Field>
+							</Form>
 
-				<Button onClick={() => this.getListOfCurrencies()} secondary>
-					Submit
-				</Button>
-
-				<Container textAlign="center">
-					<Table celled>
-						<Table.Header>
-							<Table.Row>
-								<Table.HeaderCell>Id</Table.HeaderCell>
-								<Table.HeaderCell>Image</Table.HeaderCell>
-								<Table.HeaderCell>Price</Table.HeaderCell>
-								<Table.HeaderCell>Price last 24h</Table.HeaderCell>
-								<Table.HeaderCell>No</Table.HeaderCell>
-								<Table.HeaderCell>Your balance</Table.HeaderCell>
-							</Table.Row>
-						</Table.Header>
-						<Table.Body>
-							{this.state.activeListOfCurrencies.map((crypto, id) => {
-								return (
-									<Table.Row key={id + 1}>
-										<Table.Cell> {crypto.id}</Table.Cell>
-										<Table.Cell>
-											<Image src={crypto.image} size="small" />
-										</Table.Cell>
-										<Table.Cell> ${crypto.current_price.toFixed(2)}</Table.Cell>
-										<Table.Cell>
-											{crypto.price_change_percentage_24h < 0 ? (
-												<span style={{ color: 'red' }}>
-													{crypto.price_change_percentage_24h.toFixed(2)}%
-												</span>
-											) : (
-												<span style={{ color: 'green' }}>
-													{crypto.price_change_percentage_24h.toFixed(2)}%
-												</span>
-											)}
-										</Table.Cell>
-										<Table.Cell>
-											<Form
-												onSubmit={e =>
-													this.handleOnSubmit(e, crypto.current_price)
-												}>
-												<Input
-													placeholder="Quantity"
-													name="current"
-													type="number"
-													onFocus={() => this.setState({ selected: crypto.id })}
-													onChange={this.handleOnChnage}
-												/>
-
-												<Button
-													animated
-													disabled={
-														this.state.selected === crypto.id ? false : true
-													}>
-													<Button.Content visible>Submit</Button.Content>
-													<Button.Content hidden>
-														<Icon name="arrow right" />
-													</Button.Content>
-												</Button>
-											</Form>
-										</Table.Cell>
-										<Table.Cell>
-											{this.state.myBalance.map((el, id) => {
-												if (el.id === crypto.id) {
-													return <span key={id + 1}>{el.balance}</span>;
-												}
-											})}
-										</Table.Cell>
+							<Table celled>
+								<Table.Header>
+									<Table.Row>
+										<Table.HeaderCell>Id</Table.HeaderCell>
+										<Table.HeaderCell>Image</Table.HeaderCell>
+										<Table.HeaderCell>Price</Table.HeaderCell>
+										<Table.HeaderCell>Price last 24h</Table.HeaderCell>
+										<Table.HeaderCell>No</Table.HeaderCell>
+										<Table.HeaderCell>Your balance</Table.HeaderCell>
 									</Table.Row>
-								);
-							})}
-						</Table.Body>
-					</Table>
-				</Container>
-			</div>
+								</Table.Header>
+								<Table.Body>
+									{this.state.activeListOfCurrencies.map((crypto, id) => {
+										return (
+											<Table.Row key={id + 1}>
+												<Table.Cell> {crypto.id}</Table.Cell>
+												<Table.Cell>
+													<Image src={crypto.image} size="small" />
+												</Table.Cell>
+												<Table.Cell>
+													{' '}
+													${crypto.current_price.toFixed(2)}
+												</Table.Cell>
+												<Table.Cell>
+													{crypto.price_change_percentage_24h < 0 ? (
+														<span style={{ color: 'red' }}>
+															{crypto.price_change_percentage_24h.toFixed(2)}%
+														</span>
+													) : (
+														<span style={{ color: 'green' }}>
+															{crypto.price_change_percentage_24h.toFixed(2)}%
+														</span>
+													)}
+												</Table.Cell>
+												<Table.Cell>
+													<Form
+														onSubmit={e =>
+															this.handleOnSubmit(e, crypto.current_price)
+														}>
+														<Input
+															placeholder="Quantity"
+															name="current"
+															type="number"
+															onFocus={() =>
+																this.setState({ selected: crypto.id })
+															}
+															onChange={this.handleOnChnage}
+														/>
+
+														<Button
+															animated
+															color="teal"
+															disabled={
+																this.state.selected === crypto.id ? false : true
+															}>
+															<Button.Content visible>Submit</Button.Content>
+															<Button.Content hidden>
+																<Icon name="arrow right" />
+															</Button.Content>
+														</Button>
+													</Form>
+												</Table.Cell>
+												<Table.Cell>
+													{this.state.myBalance.map((el, id) => {
+														if (el.id === crypto.id) {
+															return <span key={id + 1}>{el.balance}</span>;
+														}
+													})}
+												</Table.Cell>
+											</Table.Row>
+										);
+									})}
+								</Table.Body>
+							</Table>
+						</>
+					)}
+				</div>
+			</Container>
 		);
 	}
 }
